e1bfa4a8cbfbce046495a0555b1ec07a87fcdf3b
gskeates <gskeates@ualberta.ca>
2021-01-28 21:10:18 -0500

    Passes more tests

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +29,55 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
+        # Receive data from client
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        # Parse request for type of request and relative path to file
+        method = self.data.split()[0].decode('utf-8')
+
+        if method == 'GET':
+            # Parse data
+            file_name = self.data.split()[1].decode('utf-8')
+            path_to_file = './www' + file_name
+
+            # For 301 handling
+            redirect = False
+
+            # Serve index.html if given a directory
+            if os.path.isdir(path_to_file):
+                if path_to_file[-1] == '/':
+                    path_to_file += 'index.html'
+                else:
+                    path_to_file += '/index.html'
+                    redirect = True
+
+            # Check if path exists
+            if os.path.exists(path_to_file):
+
+                # Open and read data from file
+                file = open(path_to_file, "r")
+                file_contents = file.read()
+                file.close()
+
+                # Send file contents to client
+                if not redirect:
+                    content_type = path_to_file.split('.')[-1].strip()
+                    server_response = 'HTTP/1.1 200 OK\r\nContent-Type: text/{}\r\n\r\n{}'.format(content_type, file_contents)
+                # Redirect and send file contents
+                else:
+                    server_response = 'HTTP/1.1 301 Moved Permanently\r\nLocation: {}/\r\n\r\n'.format(file_name, file_contents)
+
+            # File doesn't exist
+            else:
+                server_response = 'HTTP/1.1 404 Not Found\r\n'
+
+        # Only serving GET requests
+        else:
+            server_response = 'HTTP/1.1 405 Method Not Allowed\r\n'
+        self.request.sendall(bytearray(server_response, 'utf-8'))
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
